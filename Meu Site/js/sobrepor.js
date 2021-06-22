fb9bc74e7ae269bc54813b7f9b0f381c72aff58b
Guilherme Santos Machado <37380202+Guilherme-Machado1@users.noreply.github.com>
2021-06-22 11:31:18 -0300

    Commit

---
+++ Meu Site/js/sobrepor.js
@@ -0,0 +1,89 @@
+function sobreposicao(cordiv){
+  document.getElementsByName('produto')[0].style.background = cordiv
+}
+function semSobreposicao(cordiv){
+  document.getElementsByName('produto')[0].style.background = cordiv
+}
+
+function sobreposicao1(cordiv){
+  document.getElementsByName('produto')[1].style.background = cordiv
+}
+function semSobreposicao1(cordiv){
+  document.getElementsByName('produto')[1].style.background = cordiv
+}
+
+function sobreposicao2(cordiv){
+  document.getElementsByName('produto')[2].style.background = cordiv
+}
+function semSobreposicao2(cordiv){
+  document.getElementsByName('produto')[2].style.background = cordiv
+}
+
+function sobreposicao3(cordiv){
+  document.getElementsByName('produto')[3].style.background = cordiv
+}
+function semSobreposicao3(cordiv){
+  document.getElementsByName('produto')[3].style.background = cordiv
+}
+
+
+
+//2º COLUMN GAMES XBOX
+function sobreposicao4(cordiv){
+  document.getElementsByName('produto')[4].style.background = cordiv
+}
+function semSobreposicao4(cordiv){
+  document.getElementsByName('produto')[4].style.background = cordiv
+}
+
+function sobreposicao5(cordiv){
+  document.getElementsByName('produto')[5].style.background = cordiv
+}
+function semSobreposicao5(cordiv){
+  document.getElementsByName('produto')[5].style.background = cordiv
+}
+
+function sobreposicao6(cordiv){
+  document.getElementsByName('produto')[6].style.background = cordiv
+}
+function semSobreposicao6(cordiv){
+  document.getElementsByName('produto')[6].style.background = cordiv
+}
+
+function sobreposicao7(cordiv){
+  document.getElementsByName('produto')[7].style.background = cordiv
+}
+function semSobreposicao7(cordiv){
+  document.getElementsByName('produto')[7].style.background = cordiv
+}
+
+
+
+//3º COLUMN GAMES PRODUTOS
+function sobreposicao8(cordiv){
+  document.getElementsByName('produto')[8].style.background = cordiv
+}
+function semSobreposicao8(cordiv){
+  document.getElementsByName('produto')[8].style.background = cordiv
+}
+
+function sobreposicao9(cordiv){
+  document.getElementsByName('produto')[9].style.background = cordiv
+}
+function semSobreposicao9(cordiv){
+  document.getElementsByName('produto')[9].style.background = cordiv
+}
+
+function sobreposicao10(cordiv){
+  document.getElementsByName('produto')[10].style.background = cordiv
+}
+function semSobreposicao10(cordiv){
+  document.getElementsByName('produto')[10].style.background = cordiv
+}
+
+function sobreposicao11(cordiv){
+  document.getElementsByName('produto')[11].style.background = cordiv
+}
+function semSobreposicao11(cordiv){
+  document.getElementsByName('produto')[11].style.background = cordiv
+}
